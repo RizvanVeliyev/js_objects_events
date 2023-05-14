9fe90d620b12c54f1eb116bff68b0fd284a0d2c4
RizvanVeliyev <veliyevrizvan250@gmail.com>
2023-05-14 09:44:39 +0400

    1

---
+++ script.js
@@ -0,0 +1,20 @@
+function addLi()
+{
+
+    var txtVal = document.getElementById('txtVal').value,
+        listNode = document.getElementById('list'),
+        liNode = document.createElement("LI"),
+        txtNode = document.createTextNode(txtVal);
+
+     liNode.appendChild(txtNode);
+     listNode.appendChild(liNode);
+
+}
+let display=document.getElementById('display');
+let list=document.getElementById('list');
+function displayList(){
+    list.classList.toggle('remove');
+        
+}
+
+display.addEventListener("click",displayList);
